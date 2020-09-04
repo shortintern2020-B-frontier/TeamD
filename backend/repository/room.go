020fcccb51f39fe697bc0fe68408f94bc387f3e5
yk-amarly-20 <y.k.83600260@icloud.com>
2020-09-04 16:28:26 +0900

    fix: unused import

--- backend/repository/room.go
+++ backend/repository/room.go
@@ -1,15 +1,13 @@
 package repository
 
 import (
-    "database/sql"
-
     "github.com/jmoiron/sqlx"
 
     "github.com/shortintern2020-B-frontier/TeamD/model"
 )
 
 // データベースからroom dataをselect all.
-func AllRooms(db *sqlx.db) ([]model.Room, error) {
+func AllRooms(db *sqlx.DB) ([]model.Room, error) {
     rooms := make([]model.Room, 0)
 
     if err := db.Select(&rooms, "select id, title, image_url from room"); err != nil {
