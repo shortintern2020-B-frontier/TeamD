dfe91830150a380334123b0e5796daa3f8eb1b2e
Ueeek <ueki@UekinoMacBook-Air.local>
2020-09-04 16:08:12 +0900

    test

--- web/src/page/home.tsx
+++ web/src/page/home.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "../css/home.module.css";
 
 const HomePage = () => {
-  return <div />;
+  return (
+  <div className={styles.sidenav}>
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#">Clients</a>
+    <a href="#">Contact</a>
+  </div>);
 };
 
 export default HomePage;
