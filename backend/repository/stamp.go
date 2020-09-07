b8af20e258ec920d8d39f6bc909b296982b31a80
yk-amarly-20 <y.k.83600260@icloud.com>
2020-09-07 16:52:55 +0900

    fix: スタンプの投稿時間によって処理変更

--- backend/repository/stamp.go
+++ backend/repository/stamp.go
@@ -8,7 +8,7 @@ import (
 )
 
 // データベースから
-func SelectStamps(db *sqlx.DB, ellapsed_time int, room_id int) ([]model.Stamp, error) {
+func SelectStamps(db *sqlx.DB, ellapsed_time int, room_id int, end_time int) ([]model.Stamp, error) {
     stamps := make([]model.Stamp, 0) 
 
  		// 最終的なやつ
@@ -17,27 +17,54 @@ func SelectStamps(db *sqlx.DB, ellapsed_time int, room_id int) ([]model.Stamp, e
         return nil, err
     }
 		*/
-
+     
+		/*
 		// テストでとりあえずこっち
 		if err := db.Select(&stamps, "select stamp_id from feeling where room_id = ? and ellapsed_time = ?", room_id, ellapsed_time); err != nil {
 			  fmt.Printf("%s", err)
 			  return nil, err
 		}
+		*/
+
+		SQLSent, flag := GenerateSQLSent(ellapsed_time, room_id, end_time)
+    
+		switch flag {
+			  case true: 
+				    if err := db.Select(&stamps, SQLSent, room_id, ellapsed_time); err != nil {
+			          fmt.Printf("%s", err)
+				        return nil, err
+		        }
+			  case false:
+					  if err := db.Select(&stamps, SQLSent, room_id, ellapsed_time, ellapsed_time); err != nil {
+							  fmt.Printf("%s", err)
+								return nil, err
+						}
+		}
 	
     return stamps, nil
 }
 
-/*
-func GenerateSQLSent(db *sqlx.DB, ellapsed_time int, room_id int) (string, error) {
+
+func GenerateSQLSent(ellapsed_time int, room_id int, end_time int) (string, bool) {
     // SQL文を生成
 		// その際、エラーがあればメッセージ投げる
-		
-		rooms := make([]model.Room, 0)
+    flag := true		// 条件分岐 上二つどちらかならtrue, 下ならfalse
 
-		if err := db.Select(&rooms, "select room_id, end_time from room where room_id = ?", room_id); err != nil {
-			  return nil, err
-	  }
+		SQLSent := "select stamp_id from feeling where room_id = ? and "
+    fmt.Printf("%d", end_time)
+		// fmt.Printf("%d", ellapsed_time)
 
-    
+    if ellapsed_time - 2000 <= 0 {
+			  fmt.Println("Yes") 
+		    SQLSent = SQLSent + "ellapsed_time >= 0 and ellapsed_time <= ? + 2000"
+    } else if ellapsed_time + 2000 >= end_time {
+			  
+			  SQLSent = SQLSent + "ellapsed_time >= ? - 2000 and ellapsed_time <= end_time"
+		} else {
+        flag = false
+			  SQLSent = SQLSent + "ellapsed_time >= ? - 2000 and ellapsed_time <= ? + 2000"
+		}
+
+		return SQLSent, flag
 }	
-*/
+
