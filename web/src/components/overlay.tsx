0a17a1fb5ca3e88d48b2951a675262ff508e1c4c
seito2 <seito1201@gmail.com>
2020-09-07 14:01:33 +0900

    [add] スタンプをmapで表示

--- web/src/components/overlay.tsx
+++ web/src/components/overlay.tsx
@@ -9,6 +9,37 @@ interface Stamp {
   ptime: number;
 }
 
+const stamps = [
+  {
+    stamp_id: 1,
+    img_url: "https://emojis.wiki/emoji-pics/apple/clapping-hands-apple.png",
+  },
+  {
+    stamp_id: 2,
+    img_url:
+      "https://i.pinimg.com/originals/71/ea/47/71ea470cde8de51e87e9c84d0a0bf7f9.png",
+  },
+  {
+    stamp_id: 3,
+    img_url:
+      "https://pics.prcm.jp/7fbe179d932a9/83247844/png/83247844_220x220.png",
+  },
+  {
+    stamp_id: 4,
+    img_url:
+      "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/114/multiple-musical-notes_1f3b6.png",
+  },
+  {
+    stamp_id: 5,
+    img_url:
+      "https://i.pinimg.com/originals/81/46/85/8146853e8ea68e606571fa8af44ca65c.png",
+  },
+  {
+    stamp_id: 6,
+    img_url: "https://www.emojiall.com/images/120/apple/1f97a.png",
+  },
+];
+
 const Overlay = (): JSX.Element => {
   const [count, setCount] = useState(0);
   const [stamp, setStamp] = useState({} as Stamp);
@@ -18,8 +49,24 @@ const Overlay = (): JSX.Element => {
     setCount(count + 1);
   };
 
-  const handleOnClickStamp = () => {
-    setStamp({ stamp_id: 1, ptime: 12 });
+  const handleOnClickStamp = (id: number) => {
+    const data = { stamp_id: id, ptime: 12 };
+    // postStampData(data);
+    setStamp({ stamp_id: id, ptime: 12 });
+  };
+
+  const postStampData = async (data: Stamp) => {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    const options: RequestInit = {
+      headers: headers,
+      method: "POST",
+      body: JSON.stringify(data),
+      mode: "cors",
+    };
+
+    const res = await fetch(`http://localhost:1996/${id}/feeling`, options);
   };
 
   return (
@@ -30,64 +77,26 @@ const Overlay = (): JSX.Element => {
         ＜
       </button>
 
-      <div>
-        {/*  /api/room/{id}/feeling */}
+      <span></span>
+
+      {/* <div>
         {count} {stamp.stamp_id} {stamp.ptime}
-      </div>
+      </div> */}
 
-      <button className={Style.button}>
-        <img
-          className={Style.image}
-          src="https://emojis.wiki/emoji-pics/apple/clapping-hands-apple.png"
-          alt="new"
-          onClick={handleOnClickStamp}
-        />
-      </button>
-
-      <button className={Style.button}>
-        <img
-          className={Style.image}
-          src="https://i.pinimg.com/originals/71/ea/47/71ea470cde8de51e87e9c84d0a0bf7f9.png"
-          alt="new"
-          onClick={handleOnClickStamp}
-        />
-      </button>
-
-      <button className={Style.button}>
-        <img
-          className={Style.image}
-          src="https://pics.prcm.jp/7fbe179d932a9/83247844/png/83247844_220x220.png"
-          alt="new"
-          onClick={handleOnClickCount}
-        />
-      </button>
-
-      <button className={Style.button}>
-        <img
-          className={Style.image}
-          src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/114/multiple-musical-notes_1f3b6.png"
-          alt="new"
-          onClick={handleOnClickCount}
-        />
-      </button>
-
-      <button className={Style.button}>
-        <img
-          className={Style.image}
-          src="https://i.pinimg.com/originals/81/46/85/8146853e8ea68e606571fa8af44ca65c.png"
-          alt="new"
-          onClick={handleOnClickCount}
-        />
-      </button>
-
-      <button className={Style.button}>
-        <img
-          className={Style.image}
-          src="https://www.emojiall.com/images/120/apple/1f97a.png"
-          alt="new"
-          onClick={handleOnClickCount}
-        />
-      </button>
+      {stamps.map(({ stamp_id, img_url }) => {
+        return (
+          <div className={Style.wrapper}>
+            <button className={Style.button}>
+              <img
+                className={Style.image}
+                src={img_url}
+                alt="new"
+                onClick={() => handleOnClickStamp(stamp_id)}
+              />
+            </button>
+          </div>
+        );
+      })}
 
       <div></div>
     </div>
