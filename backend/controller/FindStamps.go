754a85fae6d3a4c69fa2c50ebc2a14f610c0643d
yk-amarly-20 <y.k.83600260@icloud.com>
2020-09-07 16:10:12 +0900

    fix: roomが存在しないときの処理を実装

--- backend/controller/FindStamps.go
+++ backend/controller/FindStamps.go
@@ -1,11 +1,13 @@
 package controller
 
 import (
+	  "fmt"
 		"net/http"
 	  "strconv"
 
 		"github.com/jmoiron/sqlx"
     "github.com/gorilla/mux"
+	  "github.com/shortintern2020-B-frontier/TeamD/model"
 		"github.com/shortintern2020-B-frontier/TeamD/repository"
 )
 
@@ -25,6 +27,24 @@ func (stamp *Stamp) FindStamps(w http.ResponseWriter, r *http.Request) (int, int
     ellapsed_time, _ := strconv.Atoi(r.FormValue("ellapsed_time"))
 		vars := mux.Vars(r)
     room_id, _ := strconv.Atoi(vars["room_id"])
+
+		rooms := make([]model.RoomTemp, 0)
+		if err := stamp.db.Select(&rooms, "select id, end_time from room where id = ?", room_id); err != nil {
+			  fmt.Printf("%s", err)
+			  return http.StatusInternalServerError, nil, err
+		}
+
+		switch {
+			  
+			  case len(rooms) <= 0:
+					  return http.StatusNotFound, nil, nil
+
+				case len(rooms) >= 2:
+					  return http.StatusInternalServerError, nil, nil
+    }
+
+		// room := rooms[0]
+	  // _, end_time = room.RoomID, room.EndTime
 
 		stamps, err := repository.SelectStamps(stamp.db, ellapsed_time, room_id)
 
