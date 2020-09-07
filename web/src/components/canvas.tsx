da73423d58695e0dcd2356f2816336b808750f5b
seito2 <seito1201@gmail.com>
2020-09-07 10:05:53 +0900

    use class

--- web/src/components/canvas.tsx
+++ web/src/components/canvas.tsx
@@ -5,97 +5,114 @@ interface size {
   height: number;
 }
 
-const drawPeople = (
-  context: CanvasRenderingContext2D,
-  left: number,
-  top: number
-): void => {
-  context.beginPath();
-  context.moveTo(left, top);
-  context.lineTo(left - 50, top - 150);
-  context.lineTo(left - 100, top);
-  context.closePath();
+class CanvasDrawing {
+  private context: CanvasRenderingContext2D;
+  private vram: number[][];
+  private stageHeight: number;
+  private cell = { width: 70, height: 70 };
 
-  context.strokeStyle = "black";
-  context.stroke();
+  constructor(context: CanvasRenderingContext2D, stageHeight: number) {
+    this.stageHeight = stageHeight;
+    this.vram = this.createVram(stageHeight);
+    this.context = context;
+    this.drawStage();
+  }
 
-  context.fillStyle = "black";
-  context.fill();
+  drawStage = (): void => {
+    const color = "#B3B7AC";
 
-  context.beginPath();
-  context.arc(left - 50, top - 150, 30, 0, 2 * Math.PI);
-  context.closePath();
-  context.fill();
-};
+    // ステージ部分の描画
+    this.context.fillStyle = "#B3B7AC";
+    this.context.fillRect(
+      window.innerWidth * 0.1,
+      0,
+      window.innerWidth * 0.8,
+      this.stageHeight
+    );
 
-const drawStage = (
-  context: CanvasRenderingContext2D,
-  stageHeight: number
-): void => {
-  const color = "#B3B7AC";
+    // 右の線の描画
+    this.context.moveTo(window.innerWidth * 0.1, this.stageHeight);
+    this.context.lineTo(0, this.stageHeight + 120);
+    this.context.strokeStyle = color;
+    this.context.lineWidth = 2;
+    this.context.stroke();
 
-  // ステージ部分の描画
-  context.fillStyle = "#B3B7AC";
-  context.fillRect(
-    window.innerWidth * 0.1,
-    0,
-    window.innerWidth * 0.8,
-    stageHeight
-  );
+    // 左の線の描画
+    this.context.moveTo(window.innerWidth * 0.9, this.stageHeight);
+    this.context.lineTo(window.innerWidth, this.stageHeight + 120);
+    this.context.strokeStyle = color;
+    this.context.lineWidth = 2;
+    this.context.stroke();
+  };
 
-  // 右の線の描画
-  context.moveTo(window.innerWidth * 0.1, stageHeight);
-  context.lineTo(0, stageHeight + 120);
-  context.strokeStyle = color;
-  context.lineWidth = 2;
-  context.stroke();
+  drawPeople = (): void => {
+    const targetPosition = this.searchEmptyCell();
+    console.log(targetPosition);
+    if (!targetPosition) return;
+    const { x, y } = targetPosition;
+    const vram = this.vram;
+    vram[1][10] = 1;
+    this.vram = vram;
+    console.table(vram);
+    const left = window.innerWidth - this.cell.width * x;
+    const top = window.innerHeight - this.cell.height * y;
 
-  // 左の線の描画
-  context.moveTo(window.innerWidth * 0.9, stageHeight);
-  context.lineTo(window.innerWidth, stageHeight + 120);
-  context.strokeStyle = color;
-  context.lineWidth = 2;
-  context.stroke();
-};
+    this.context.beginPath();
+    this.context.moveTo(left, top);
+    this.context.lineTo(left - 50, top - 150);
+    this.context.lineTo(left - 100, top);
+    this.context.closePath();
 
-const createVram = (stageHeight: number): number[][] => {
-  const cell = { width: 70, height: 70 };
+    this.context.strokeStyle = "black";
+    this.context.stroke();
 
-  const horizontalCellLength = Math.floor(window.innerWidth / cell.width);
-  const vertivalCellLength = Math.floor(
-    (window.innerHeight - stageHeight - 60) / cell.height
-  );
+    this.context.fillStyle = "black";
+    this.context.fill();
 
-  const horizontalCell = Array.apply(null, new Array(horizontalCellLength)).map(
-    Number.prototype.valueOf,
-    0
-  );
+    this.context.beginPath();
+    this.context.arc(left - 50, top - 150, 30, 0, 2 * Math.PI);
+    this.context.closePath();
+    this.context.fill();
+  };
 
-  return [...Array(vertivalCellLength)].map(() => horizontalCell);
-};
+  createVram = (stageHeight: number): number[][] => {
+    const horizontalCellLength = Math.floor(
+      window.innerWidth / this.cell.width
+    );
+    const vertivalCellLength = Math.floor(
+      (window.innerHeight - stageHeight - 60) / this.cell.height
+    );
 
-const searchEmptyCell = (vram: number[][]) => {
-  return vram
-    .map((horizontalCells, y) =>
-      horizontalCells.map((cell, x) => (cell !== 1 ? { x, y } : undefined))
-    )
-    .flat()
-    .filter(Boolean);
-};
+    const horizontalCell = Array.apply(
+      null,
+      new Array(horizontalCellLength)
+    ).map(Number.prototype.valueOf, 0);
 
-const randomlyGetValuesFromArray = <T extends unknown>(array: T[]): T => {
-  return array[Math.floor(Math.random() * array.length)];
-};
+    return [...Array(vertivalCellLength)].map(() => horizontalCell);
+  };
+
+  searchEmptyCell = () => {
+    const emptyCells = this.vram
+      .map((horizontalCells, y) =>
+        horizontalCells.map((cell, x) => (cell !== 1 ? { x, y } : undefined))
+      )
+      .flat()
+      .filter(Boolean);
+    console.table(this.vram);
+    return this.randomlyGetValuesFromArray(emptyCells);
+  };
+
+  randomlyGetValuesFromArray = <T extends unknown>(array: T[]): T => {
+    return array[Math.floor(Math.random() * array.length)];
+  };
+}
 
 const Canvas = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [windowSize, setWindowSize] = useState({} as size);
+  const [canvasDrawing, setCanvasDrawing] = useState<CanvasDrawing>();
 
   const stageHeight = 400;
-
-  let vram: number[][] = createVram(stageHeight);
-  const emptyCells = searchEmptyCell(vram);
-  const targetCell = randomlyGetValuesFromArray(emptyCells);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -103,8 +120,8 @@ const Canvas = () => {
     const context = canvas.getContext("2d");
     if (!context) return;
 
-    drawStage(context, stageHeight);
-    drawPeople(context, window.innerWidth - 100, window.innerHeight - 100);
+    setCanvasDrawing(new CanvasDrawing(context, stageHeight));
+    // drawStage(this.context, stageHeight);
   }, [windowSize]);
 
   useEffect(() => {
@@ -125,7 +142,8 @@ const Canvas = () => {
     e: React.MouseEvent<HTMLCanvasElement, MouseEvent>
   ) => {
     console.log(e.clientX);
-    console.log(targetCell);
+    if (!canvasDrawing) return;
+    canvasDrawing.drawPeople();
   };
 
   return (
