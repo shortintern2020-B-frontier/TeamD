7c1ae6ccc94489f7fb7279adf76532a0208875e3
mmmmmeif <mmmmmeif@.com>
2020-09-07 09:57:36 +0900

    WIG

--- web/src/page/home.tsx
+++ web/src/page/home.tsx
@@ -3,8 +3,6 @@ import Sidebar from "../components/sidebar";
 import styles from '../css/home.module.css';
 
 const HomePage = () => {
-  console.log(styles)
-  console.log('hello')
   return (
     <div className={styles.above}>
       <Sidebar/>
