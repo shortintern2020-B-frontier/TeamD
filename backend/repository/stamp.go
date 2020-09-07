3568e7cdef31e55ad46a1c43957f7a21a9be7e68
yk-amarly-20 <y.k.83600260@icloud.com>
2020-09-07 16:57:20 +0900

    fix: いらないとこ削除

--- backend/repository/stamp.go
+++ backend/repository/stamp.go
@@ -10,34 +10,20 @@ import (
 // データベースから
 func SelectStamps(db *sqlx.DB, ellapsed_time int, room_id int, end_time int) ([]model.Stamp, error) {
     stamps := make([]model.Stamp, 0) 
-
- 		// 最終的なやつ
-		/*
-    if err := db.Select(&stamps, "select stamp_id from feeling where room_id = ${room_id} and ellapsed_time > ${ellapsed_time} - 2000 and ellapsed_time < ${ellapsed_time} + 2000"); err != nil {
-        return nil, err
-    }
-		*/
-     
-		/*
-		// テストでとりあえずこっち
-		if err := db.Select(&stamps, "select stamp_id from feeling where room_id = ? and ellapsed_time = ?", room_id, ellapsed_time); err != nil {
-			  fmt.Printf("%s", err)
-			  return nil, err
-		}
-		*/
-
+  
 		SQLSent, flag := GenerateSQLSent(ellapsed_time, room_id, end_time)
     
+		// 埋め込み引数の個数で分岐
 		switch flag {
 			  case true: 
 				    if err := db.Select(&stamps, SQLSent, room_id, ellapsed_time); err != nil {
-			          fmt.Printf("%s", err)
+			         
 				        return nil, err
 		        }
 			  case false:
 					  if err := db.Select(&stamps, SQLSent, room_id, ellapsed_time, ellapsed_time); err != nil {
-							  fmt.Printf("%s", err)
-								return nil, err
+						
+							return nil, err
 						}
 		}
 	
@@ -47,13 +33,11 @@ func SelectStamps(db *sqlx.DB, ellapsed_time int, room_id int, end_time int) ([]
 
 func GenerateSQLSent(ellapsed_time int, room_id int, end_time int) (string, bool) {
     // SQL文を生成
-		// その際、エラーがあればメッセージ投げる
-    flag := true		// 条件分岐 上二つどちらかならtrue, 下ならfalse
+		flag := true		// 条件分岐 上二つどちらかならtrue, 下ならfalse
 
 		SQLSent := "select stamp_id from feeling where room_id = ? and "
     fmt.Printf("%d", end_time)
-		// fmt.Printf("%d", ellapsed_time)
-
+		
     if ellapsed_time - 2000 <= 0 {
 			  fmt.Println("Yes") 
 		    SQLSent = SQLSent + "ellapsed_time >= 0 and ellapsed_time <= ? + 2000"
