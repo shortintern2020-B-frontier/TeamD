fb57d482e9cddc61432f83567d156f0dd95c7b45
mmmmmeif <mmmmmeif@.com>
2020-09-07 09:57:36 +0900

    WIG

--- web/src/components/sidebar.tsx
+++ web/src/components/sidebar.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 
 import Style from "../css/sidebar.module.css";
+import { FiSettings } from "react-icons/fi";
+import { FiFilm } from "react-icons/fi";
+import { FiLogOut } from "react-icons/fi";
 
 const Sidebar = () =>{
     return(
-        <div className={Style.sidenav}>
-            <a href="#">About</a>
-            <a href="#">Services</a>
-            <a href="#">Clients</a>
-            <a href="#">Contact</a>
+        <div>
+            <div className={Style.shutter}></div>
+            <section className={Style.container}>
+                <div className={Style.sidenav}>
+                    <a href="#"><FiFilm /></a>
+                    <a href="#"><FiSettings /></a>
+                    <br/>
+                    <br/>
+                    <a href="#"><FiLogOut /></a>
+                </div>
+            </section>
         </div>
   );
 };
