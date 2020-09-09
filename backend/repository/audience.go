0fc7d37ebc9bf506d06f3c697dc9db3ff822193c
t-hoso <taishihosokawa@yahoo.co.jp>
2020-09-09 10:06:27 +0900

    fix json return format

--- backend/repository/audience.go
+++ backend/repository/audience.go
@@ -2,11 +2,13 @@ package repository
 
 import (
 	"github.com/jmoiron/sqlx"
+
+	"github.com/shortintern2020-B-frontier/TeamD/model"
 )
 
 // return audience.count
-func FindAudience (db *sqlx.DB, id, ellapsed_time int) (*int, error) {
-	var audience int
+func FindAudience (db *sqlx.DB, id, ellapsed_time int) (*model.Audience, error) {
+	var audience model.Audience
 	if err := db.Get(&audience, `
 	SELECT count FROM audience WHERE room_id = ? AND ellapsed_time = ?
 	`, id, ellapsed_time); err != nil {
