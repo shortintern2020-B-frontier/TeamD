74fd697aea06ab5e58d7617349b8e0fa0982c470
t-hoso <taishihosokawa@yahoo.co.jp>
2020-09-09 11:11:44 +0900

    add migration

---
+++ database/migration/schema/V6__create_procedure_insert_audience.sql
@@ -0,0 +1,23 @@
+DROP PROCEDURE IF EXISTS insert_audience;
+DELIMITER //
+CREATE PROCEDURE insert_audience(IN room_id INT, IN start_time INT)
+
+BEGIN
+  DECLARE end_time INT DEFAULT 0;
+  DECLARE end_times CURSOR FOR
+    SELECT end_time FROM room WHERE id = room_id;
+  OPEN end_times;
+  FETCH end_times INTO end_time;
+  CLOSE end_times;
+
+  DECLARE i INT DEFAULT start_time / 60000;
+  WHILE i <= end_time / 60000 DO
+  INSERT INTO audience (room_id, ellapsed_time, count) VALUES (room_id, i*60000, 0);
+  END WHILE;
+END
+//
+DELIMITER ;
+
+
+call insert_audience(1, 120000);
+call insert_audience(2, 60000);
