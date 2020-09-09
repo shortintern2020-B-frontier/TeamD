56d39e6fbcd6b208cf1a4d3550f0f000ed6b4d1c
t-hoso <taishihosokawa@yahoo.co.jp>
2020-09-09 16:55:21 +0900

    add procedure and add audience record

--- database/migration/schema/V7__create_procedure_insert_audience.sql
+++ database/migration/schema/V7__create_procedure_insert_audience.sql
@@ -1,20 +1,20 @@
 DROP PROCEDURE IF EXISTS insert_audience;
 DELIMITER //
 CREATE PROCEDURE insert_audience(IN room_id INT, IN start_time INT)
-DECLARE e INT DEFAULT 0;
 
 BEGIN
+  DECLARE i INT DEFAULT start_time DIV 60000;
   DECLARE end_time INT DEFAULT 0;
   DECLARE end_times CURSOR FOR
-    SELECT end_time FROM room WHERE id = room_id;
+    SELECT roomA.end_time FROM room AS roomA WHERE id = room_id;
   OPEN end_times;
   FETCH end_times INTO end_time;
   CLOSE end_times;
-
-  DECLARE i INT DEFAULT start_time DIV 60000;
   WHILE i < (end_time DIV 60000)+1 DO
     INSERT audience (room_id, ellapsed_time, count) VALUES(room_id, i*60000, 0);
+    set i=i+1;
   END WHILE;
+COMMIT;
 END//
 DELIMITER ;
 
