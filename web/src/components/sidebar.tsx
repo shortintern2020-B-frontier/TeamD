972a2e9adea921b32a022b6e74d87b995a07311e
Ueeek <ueki.k.ae@m.titech.ac.jp>
2020-09-09 12:05:54 +0900

    change icon

--- web/src/components/sidebar.tsx
+++ web/src/components/sidebar.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 
 import Style from "../css/sidebar.module.css";
-import { FiSettings,FiFilm,FiLogOut } from "react-icons/fi";
+import { FiSettings,FiFilm,FiLogOut,FiBook} from "react-icons/fi";//Feather icon
 
 const Sidebar = () =>{
     return(
         <div>
             <section className={Style.container}>
                 <div className={Style.sidenav}>
-                    <a href="./Author"><FiFilm /></a>
+                    <a href="#"><FiFilm /></a>
                     <a href="#"><FiSettings /></a>
+                    <a href="./Author"><FiBook /></a>
                     <br/>
                     <br/>
                     <a href="#"><FiLogOut /></a>
