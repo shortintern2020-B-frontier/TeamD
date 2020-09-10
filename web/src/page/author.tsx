039c63ed569fe9dde971bf7744e8a459e9df3029
Ueeek <ueki.k.ae@m.titech.ac.jp>
2020-09-10 10:52:38 +0900

    change image's author name

--- web/src/page/author.tsx
+++ web/src/page/author.tsx
@@ -10,7 +10,7 @@ const AuthorPage=()=>{
       <div className={styles.container}>
         <h1 className={styles.text}>著作権関連のページ</h1>
         <div>
-          <p className={styles.text}>room(/room/room_id)の背景画像は<a href="https://jp.freepik.com/vectors/nature">Brgfx - jp.freepik.com によって作成された nature ベクトル</a>です。</p>
+          <p className={styles.text}>room(/room/room_id)の背景画像は <a href="https://jp.freepik.com/vectors/light">Macrovector - jp.freepik.com によって作成された light ベクトル</a></p>
         </div>
         <button onClick={handleOnClickBackHome}>Homeに戻る</button>
       </div>
