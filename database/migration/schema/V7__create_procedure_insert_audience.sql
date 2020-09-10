8263607eb0028058bb8340c7f90cb5cdd59e612a
t-hoso <taishihosokawa@yahoo.co.jp>
2020-09-10 09:18:27 +0900

    change variable names

--- database/migration/schema/V7__create_procedure_insert_audience.sql
+++ database/migration/schema/V7__create_procedure_insert_audience.sql
@@ -1,18 +1,20 @@
+# Written by Taishi Hosokawa
 DROP PROCEDURE IF EXISTS insert_audience;
 DELIMITER //
 CREATE PROCEDURE insert_audience(IN room_id INT, IN start_time INT)
 
 BEGIN
-  DECLARE i INT DEFAULT start_time DIV 60000;
+  DECLARE target_ellapsed_time INT DEFAULT start_time;
   DECLARE end_time INT DEFAULT 0;
-  DECLARE end_times CURSOR FOR
+  DECLARE cursor_end_time CURSOR FOR
     SELECT roomA.end_time FROM room AS roomA WHERE id = room_id;
-  OPEN end_times;
-  FETCH end_times INTO end_time;
-  CLOSE end_times;
-  WHILE i < (end_time DIV 60000)+1 DO
-    INSERT audience (room_id, ellapsed_time, count) VALUES(room_id, i*60000, 0);
-    set i=i+1;
+  OPEN cursor_end_times;
+  FETCH cursor_end_times INTO end_time;
+  CLOSE cursor_end_times;
+  
+  WHILE target_ellapsed_time <= end_time DO
+    INSERT audience (room_id, ellapsed_time, count) VALUES(room_id, target_ellapsed_time, 0);
+    set target_ellapsed_time = target_ellapsed_time + 60000;
   END WHILE;
 COMMIT;
 END//
